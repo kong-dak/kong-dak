8a19e482fdd9a1f971331c108311e9cc2911463f
Changho0514 <chh9450@naver.com>
2024-11-28 18:38:14 +0900

    feat: 초기 backend 세팅

---
+++ src/main/java/com/kongdak/KongdakApplication.java
@@ -0,0 +1,13 @@
+package com.kongdak;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KongdakApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KongdakApplication.class, args);
+	}
+
+}
