862e3241e882b620ce00d19b83c513eb444815af
pigggulggul <impigggulggul@naver.com>
2024-12-10 14:07:57 +0900

    feat: Frontend 라우터 구조 설정
    - React Native 폴더 구조화
    - Layout 연결
    Resolves:
    
    [#3](https://github.com/kong-dak/kong-dak/issues/3)

---
+++ frontend/app/(tabs)/play/PlayHome.tsx
@@ -0,0 +1,20 @@
+import { useNavigation } from "@react-navigation/native";
+import { NativeStackNavigationProp } from "@react-navigation/native-stack";
+import { View, Text, Button } from "react-native";
+
+export type RootStackParam = {
+  Home: undefined;
+};
+
+export default function PlayScreen() {
+  const navigation = useNavigation<NativeStackNavigationProp<RootStackParam>>();
+  return (
+    <View>
+      <Text>Play화면입니다</Text>
+      <Button
+        title="Go to TestScreen"
+        onPress={() => navigation.navigate("Home")}
+      />
+    </View>
+  );
+}
