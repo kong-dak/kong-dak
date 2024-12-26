6acfa940b62929e2f8730da4a62b4bfafec18863
Changho0514 <chh9450@naver.com>
2024-12-26 11:28:19 +0900

    feat : AnswerEmoji, AnswerReply, DailyQuestion, DailyAnswer 엔티티 추가

---
+++ backend/src/main/java/com/kongdak/domain/dailyquestion/AnswerEmoji.java
@@ -0,0 +1,38 @@
+package com.kongdak.domain.dailyquestion;
+
+
+import com.kongdak.domain.BaseTimeEntity;
+import com.kongdak.domain.member.Member;
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Table(name = "answer_emojis")
+public class AnswerEmoji extends BaseTimeEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "answer_id")
+    private DailyAnswer answer;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "member_id")
+    private Member member;
+
+    @Column(nullable = false)
+    private String emoji;
+
+    @Builder
+    public AnswerEmoji(DailyAnswer answer, Member member, String emoji) {
+        this.answer = answer;
+        this.member = member;
+        this.emoji = emoji;
+    }
+}
