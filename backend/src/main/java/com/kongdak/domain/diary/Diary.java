cd7e1af06d25cfc20eb1c3275f7a53442de95da9
Changho0514 <chh9450@naver.com>
2025-01-02 12:24:12 +0900

    feat : Diary 관련 엔티티 추가
    Diary, DiaryPhoto, DiaryDecoration 으로 나누어 분리하였음

---
+++ backend/src/main/java/com/kongdak/domain/diary/Diary.java
@@ -0,0 +1,93 @@
+package com.kongdak.domain.diary;
+
+import com.kongdak.domain.BaseTimeEntity;
+import com.kongdak.domain.couple.Couple;
+import com.kongdak.domain.member.Member;
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Diary extends BaseTimeEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "couple_id")
+    private Couple couple;
+
+    @Column(nullable = false)
+    private String content;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private Emotion emotion;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private Weather weather;
+
+    @Column(nullable = false)
+    private LocalDate diaryDate;
+
+    private boolean isEditing;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "editor_id")
+    private Member editor;
+
+    @OneToMany(mappedBy = "diary", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<DiaryPhoto> photos = new ArrayList<>();
+
+    @OneToMany(mappedBy = "diary", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<DiaryDecoration> decorations = new ArrayList<>();
+
+    @Builder
+    public Diary(Couple couple, String content, Emotion emotion, Weather weather, LocalDate diaryDate) {
+        this.couple = couple;
+        this.content = content;
+        this.emotion = emotion;
+        this.weather = weather;
+        this.diaryDate = diaryDate;
+    }
+
+    // 수정 메서드
+    public void update(String content, Emotion emotion, Weather weather) {
+        this.content = content;
+        this.emotion = emotion;
+        this.weather = weather;
+    }
+
+    // 편집 잠금
+    public void startEditing(Member editor) {
+        this.isEditing = true;
+        this.editor = editor;
+    }
+
+    // 편집 잠금 해제
+    public void finishEditing() {
+        this.isEditing = false;
+        this.editor = null;
+    }
+
+    // 사진 추가
+    public void addPhoto(DiaryPhoto photo) {
+        this.photos.add(photo);
+        photo.setDiary(this);
+    }
+
+    // 꾸미기 추가
+    public void addDecoration(DiaryDecoration decoration) {
+        this.decorations.add(decoration);
+        decoration.setDiary(this);
+    }
+}
