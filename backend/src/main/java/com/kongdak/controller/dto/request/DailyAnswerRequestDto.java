2bc88fcc85cea7381f3ddc9f8a0d00ff804283d1
Changho0514 <chh9450@naver.com>
2025-01-11 15:07:29 +0900

    refactor: 이름변경 RequestDto -> Request, ResponseDto -> Response 컨벤션이 Dto suffix 를 빼는 쪽이므로 빼어서 refactoring 한다.

--- backend/src/main/java/com/kongdak/controller/dto/request/DailyAnswerRequestDto.java
+++
@@ -1,6 +0,0 @@
-package com.kongdak.controller.dto.request;
-
-public record DailyAnswerRequestDto(
-        String content
-) {
-}
---
+++ backend/src/main/java/com/kongdak/controller/dto/request/EmojiRequest.java
@@ -0,0 +1,9 @@
+package com.kongdak.controller.dto.request;
+
+import io.swagger.v3.oas.annotations.media.Schema;
+
+@Schema(description = "이모지 요청")
+public record EmojiRequest(
+        @Schema(description = "이모지", example = "👍")
+        String emoji
+) {}
