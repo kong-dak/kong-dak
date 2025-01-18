dc907e703b36de523746dae22633646ae0cab772
Changho0514 <chh9450@naver.com>
2025-01-18 18:33:38 +0900

    feat: 커플 마지막 답변 조회 메서드 추가 - 커플의 데일리질문 조회 로직이 날짜 기준에서 순서(몇 번째 질문)로 바뀜에 따라 메서드 추가 - 특정 멤버의 마지막 답변 조회, 커플 모두의 답변 여부 확인 메서드 추가

--- backend/src/main/java/com/kongdak/domain/dailyquestion/DailyQuestionService.java
+++ backend/src/main/java/com/kongdak/domain/dailyquestion/DailyQuestionService.java
@@ -5,19 +5,23 @@ import com.kongdak.controller.dto.request.EmojiRequest;
 import com.kongdak.controller.dto.request.ReplyRequest;
 import com.kongdak.controller.dto.response.DailyAnswerResponse;
 import com.kongdak.controller.dto.response.DailyQuestionResponse;
+import com.kongdak.domain.couple.Couple;
+import com.kongdak.domain.couple.CoupleRepository;
 import com.kongdak.domain.member.Member;
 import com.kongdak.domain.member.MemberRepository;
 import com.kongdak.global.exception.BusinessException;
 import com.kongdak.global.exception.ErrorCode;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-import java.time.LocalDateTime;
 import java.util.List;
+import java.util.Optional;
 import java.util.stream.Collectors;
 
 @Service
+@Slf4j
 @RequiredArgsConstructor
 @Transactional(readOnly = true)
 public class DailyQuestionService {
@@ -26,16 +30,35 @@ public class DailyQuestionService {
     private final AnswerReplyRepository answerReplyRepository;
     private final AnswerEmojiRepository answerEmojiRepository;
     private final MemberRepository memberRepository;
+    private final CoupleRepository coupleRepository;
 
     // 오늘의 질문 조회
-    public DailyQuestionResponse getDailyQuestion() {
-        LocalDateTime startOfDay = LocalDateTime.now().withHour(0).withMinute(0).withSecond(0);
-        LocalDateTime endOfDay = startOfDay.plusDays(1);
+    public DailyQuestionResponse getDailyQuestion(Long memberId) {
 
-        DailyQuestion question = dailyQuestionRepository.findByCreatedAtBetween(startOfDay, endOfDay)
-                .orElseThrow(() -> new BusinessException(ErrorCode.QUESTION_NOT_FOUND));
+        // 현재 회원의 커플 정보 조회
+        Couple couple = coupleRepository.findByMemberId(memberId)
+                .orElseThrow(() -> new BusinessException(ErrorCode.COUPLE_NOT_FOUND));
 
-        return DailyQuestionResponse.from(question);
+        // 현재 회원의 마지막 답변 조회
+        Optional<DailyAnswer> lastAnswer =
+                dailyAnswerRepository.findLastAnswerByMemberId(memberId);
+        // 다음 질문 조회
+        DailyQuestion nextQuestion;
+        if (lastAnswer.isEmpty()) {
+            // 첫 번째 질문 조회
+            nextQuestion = dailyQuestionRepository.findFirstByOrderByIdAsc()
+                    .orElseThrow(() -> new BusinessException(ErrorCode.QUESTION_NOT_FOUND));
+        } else {
+            // 마지막 답변의 다음 질문 조회
+            nextQuestion = dailyQuestionRepository.findNextQuestion(lastAnswer.get().getQuestion().getId())
+                    .orElseThrow(() -> new BusinessException(ErrorCode.QUESTION_NOT_FOUND));
+        }
+
+        return DailyQuestionResponse.builder()
+                .questionId(nextQuestion.getId())
+                .title(nextQuestion.getTitle())
+                .build();
+
     }
 
     // 답변 작성
@@ -67,8 +90,11 @@ public class DailyQuestionService {
         Member member = memberRepository.findById(memberId)
                 .orElseThrow(() -> new BusinessException(ErrorCode.MEMBER_NOT_FOUND));
 
+        log.info("[DailyQuestionService - getAnswers]- memberId : {}", memberId);
         // 커플 관계 확인
         Long coupleId = member.getCoupleId();
+        log.info("[DailyQuestionService - getAnswers]- Couple 엔티티: {}", member.getCouple());
+        log.info("[DailyQuestionService - getAnswers]- coupleId : {}", coupleId);
         if (coupleId == null) {
             throw new BusinessException(ErrorCode.COUPLE_NOT_FOUND);
         }
