83c0643c542b30d3e84bf1503d96313262324319
Changho0514 <chh9450@naver.com>
2025-02-25 16:52:54 +0900

    fix : JwtTokenProvider 주석처리 후에 Spring Security로 바뀔때를 대비하여 남겨두나, 현재 충돌이 일어나므로 바꾸기로 한다.

--- backend/src/main/java/com/kongdak/global/security/jwt/JwtTokenProvider.java
+++ backend/src/main/java/com/kongdak/global/security/jwt/JwtTokenProvider.java
@@ -1,77 +1,110 @@
-package com.kongdak.global.security.jwt;
-
-import com.kongdak.global.exception.BusinessException;
-import com.kongdak.global.exception.ErrorCode;
-import io.jsonwebtoken.*;
-import io.jsonwebtoken.security.Keys;
-import io.jsonwebtoken.security.SignatureException;
-
-import lombok.Getter;
-import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.security.core.GrantedAuthority;  // 추가
-import org.springframework.security.core.authority.SimpleGrantedAuthority;  // 추가
-import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;  // 추가
-import org.springframework.security.core.Authentication;
-import org.springframework.security.oauth2.core.user.OAuth2User;
-import org.springframework.stereotype.Component;
-
-import java.security.Key;
-import java.util.Arrays;
-import java.util.Collection;
-import java.util.Date;
-import java.util.Map;
-import java.util.stream.Collectors;
-
-@Slf4j
-@Getter
-@Component
-public class JwtTokenProvider {
-
-    private final Key key;
-    private final long accessTokenValidityInMilliseconds;
-    private final long refreshTokenValidityInMilliseconds;
-    private final CustomOAuth2UserService customOAuth2UserService;
-
-
-    public JwtTokenProvider(
-            @Value("${spring.jwt.secret}") String secretKey,
-            @Value("${spring.jwt.access-token-validity}") long accessTokenValidityInSeconds,
-            @Value("${spring.jwt.refresh-token-validity}") long refreshTokenValidityInSeconds,
-            CustomOAuth2UserService customOAuth2UserService) {
-        this.key = Keys.secretKeyFor(SignatureAlgorithm.HS256);
-        this.accessTokenValidityInMilliseconds = accessTokenValidityInSeconds * 1000;
-        this.refreshTokenValidityInMilliseconds = refreshTokenValidityInSeconds * 1000;
-        this.customOAuth2UserService = customOAuth2UserService;
-    }
-
-    // Access Token 생성
-    public String createToken(OAuth2User oAuth2User) {
-        Date now = new Date();
-        Date validity = new Date(now.getTime() + accessTokenValidityInMilliseconds);
-
-        // attributes 확인을 위한 로그
-        log.info("OAuth2User attributes: {}", oAuth2User.getAttributes());
-        String email;
-        Long id;
-        if (oAuth2User instanceof CustomOAuth2User) {
-            id = ((CustomOAuth2User) oAuth2User).getId();
-            log.info("JwtTokenProvider에서 id : {}", id);
-            email = ((CustomOAuth2User) oAuth2User).getEmail();
-        } else {
-            // 기본 OAuth2User의 경우 attributes에서 id를 추출
-            Map<String, Object> attributes = oAuth2User.getAttributes();
-            if (attributes.containsKey("kakao_account")) {
-                email = ((Map<String, Object>) attributes.get("kakao_account")).get("email").toString();
-                id = Long.valueOf(attributes.get("id").toString());
-                log.info("JwtTokenProvider에서 카카오로 갔음. id : {}", id);
-            } else {
-                email = attributes.get("email").toString();
-                id = Long.valueOf(attributes.get("sub").toString());
-            }
-        }
-
-
+//package com.kongdak.global.security.jwt;
+//
+//import com.kongdak.global.exception.BusinessException;
+//import com.kongdak.global.exception.ErrorCode;
+//import io.jsonwebtoken.*;
+//import io.jsonwebtoken.security.Keys;
+//import io.jsonwebtoken.security.SignatureException;
+//
+//import lombok.Getter;
+//import lombok.extern.slf4j.Slf4j;
+//import org.springframework.beans.factory.annotation.Value;
+//import org.springframework.security.core.GrantedAuthority;  // 추가
+//import org.springframework.security.core.authority.SimpleGrantedAuthority;  // 추가
+//import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;  // 추가
+//import org.springframework.security.core.Authentication;
+//import org.springframework.security.oauth2.core.user.OAuth2User;
+//import org.springframework.stereotype.Component;
+//
+//import java.security.Key;
+//import java.util.Arrays;
+//import java.util.Collection;
+//import java.util.Date;
+//import java.util.Map;
+//import java.util.stream.Collectors;
+//
+//@Slf4j
+//@Getter
+//@Component
+//public class JwtTokenProvider {
+//
+//    private final Key key;
+//    private final long accessTokenValidityInMilliseconds;
+//    private final long refreshTokenValidityInMilliseconds;
+//    private final CustomOAuth2UserService customOAuth2UserService;
+//
+//
+//    public JwtTokenProvider(
+//            @Value("${spring.jwt.secret}") String secretKey,
+//            @Value("${spring.jwt.access-token-validity}") long accessTokenValidityInSeconds,
+//            @Value("${spring.jwt.refresh-token-validity}") long refreshTokenValidityInSeconds,
+//            CustomOAuth2UserService customOAuth2UserService) {
+//        this.key = Keys.secretKeyFor(SignatureAlgorithm.HS256);
+//        this.accessTokenValidityInMilliseconds = accessTokenValidityInSeconds * 1000;
+//        this.refreshTokenValidityInMilliseconds = refreshTokenValidityInSeconds * 1000;
+//        this.customOAuth2UserService = customOAuth2UserService;
+//    }
+//
+//    // Access Token 생성
+//    public String createToken(OAuth2User oAuth2User) {
+//        Date now = new Date();
+//        Date validity = new Date(now.getTime() + accessTokenValidityInMilliseconds);
+//
+//        // attributes 확인을 위한 로그
+//        log.info("OAuth2User attributes: {}", oAuth2User.getAttributes());
+//        String email;
+//        Long id;
+//        if (oAuth2User instanceof CustomOAuth2User) {
+//            id = ((CustomOAuth2User) oAuth2User).getId();
+//            log.info("JwtTokenProvider에서 id : {}", id);
+//            email = ((CustomOAuth2User) oAuth2User).getEmail();
+//        } else {
+//            // 기본 OAuth2User의 경우 attributes에서 id를 추출
+//            Map<String, Object> attributes = oAuth2User.getAttributes();
+//            if (attributes.containsKey("kakao_account")) {
+//                email = ((Map<String, Object>) attributes.get("kakao_account")).get("email").toString();
+//                id = Long.valueOf(attributes.get("id").toString());
+//                log.info("JwtTokenProvider에서 카카오로 갔음. id : {}", id);
+//            } else {
+//                email = attributes.get("email").toString();
+//                id = Long.valueOf(attributes.get("sub").toString());
+//            }
+//        }
+//
+//
+////        Map<String, Object> attributes = oAuth2User.getAttributes();
+////        if (attributes.containsKey("kakao_account")) {
+////            // 카카오 로그인
+////            email = ((Map<String, Object>) attributes.get("kakao_account")).get("email").toString();
+////            id = Long.valueOf(attributes.get("id").toString());
+////        } else {
+////            // 구글 로그인
+////            email = attributes.get("email").toString();
+////            id = Long.valueOf(attributes.get("sub").toString()); // Google uses 'sub' as unique identifier
+////        }
+//
+//        return Jwts.builder()
+//                .setSubject(email)
+//                .claim("id", id)
+//                .claim("auth", oAuth2User.getAuthorities().stream()
+//                        .map(GrantedAuthority::getAuthority)
+//                        .collect(Collectors.joining(",")))
+//                .setIssuedAt(now)
+//                .setExpiration(validity)
+//                .signWith(key, SignatureAlgorithm.HS256)
+//                .compact();
+//    }
+//
+//    // Refresh Token 생성
+//    public String createRefreshToken(OAuth2User oAuth2User) {
+//        Date now = new Date();
+//        Date validity = new Date(now.getTime() + refreshTokenValidityInMilliseconds);
+//
+//        // attributes 확인을 위한 로그
+//        log.info("OAuth2User attributes: {}", oAuth2User.getAttributes());
+//        String email;
+//        Long id;
+//
 //        Map<String, Object> attributes = oAuth2User.getAttributes();
 //        if (attributes.containsKey("kakao_account")) {
 //            // 카카오 로그인
@@ -82,144 +115,111 @@ public class JwtTokenProvider {
 //            email = attributes.get("email").toString();
 //            id = Long.valueOf(attributes.get("sub").toString()); // Google uses 'sub' as unique identifier
 //        }
-
-        return Jwts.builder()
-                .setSubject(email)
-                .claim("id", id)
-                .claim("auth", oAuth2User.getAuthorities().stream()
-                        .map(GrantedAuthority::getAuthority)
-                        .collect(Collectors.joining(",")))
-                .setIssuedAt(now)
-                .setExpiration(validity)
-                .signWith(key, SignatureAlgorithm.HS256)
-                .compact();
-    }
-
-    // Refresh Token 생성
-    public String createRefreshToken(OAuth2User oAuth2User) {
-        Date now = new Date();
-        Date validity = new Date(now.getTime() + refreshTokenValidityInMilliseconds);
-
-        // attributes 확인을 위한 로그
-        log.info("OAuth2User attributes: {}", oAuth2User.getAttributes());
-        String email;
-        Long id;
-
-        Map<String, Object> attributes = oAuth2User.getAttributes();
-        if (attributes.containsKey("kakao_account")) {
-            // 카카오 로그인
-            email = ((Map<String, Object>) attributes.get("kakao_account")).get("email").toString();
-            id = Long.valueOf(attributes.get("id").toString());
-        } else {
-            // 구글 로그인
-            email = attributes.get("email").toString();
-            id = Long.valueOf(attributes.get("sub").toString()); // Google uses 'sub' as unique identifier
-        }
-
-        return Jwts.builder()
-                .setSubject(email)
-                .setIssuedAt(now)
-                .setExpiration(validity)
-                .signWith(key, SignatureAlgorithm.HS256)
-                .compact();
-    }
-
-    // 토큰에서 Authentication 객체 추출
-    public Authentication getAuthentication(String token) {
-        Claims claims = parseClaims(token);
-
-        log.info("Token claims: {}", claims);
-        log.info("id claim value: {}", claims.get("id"));
-        log.info("subject value: {}", claims.getSubject());
-        log.info("auth claim value: {}", claims.get("auth"));
-
-        Collection<? extends GrantedAuthority> authorities =
-                Arrays.stream(claims.get("auth").toString().split(","))
-                        .map(SimpleGrantedAuthority::new)
-                        .collect(Collectors.toList());
-
-        CustomUserDetails principal = CustomUserDetails.builder()
-                .id(Long.parseLong(claims.get("id").toString()))
-                .email(claims.getSubject())
-                .authorities(authorities)
-                .build();
-
-        return new UsernamePasswordAuthenticationToken(principal, token, authorities);
-    }
-
-    // 토큰 유효성 검증
-    public boolean validateToken(String token) {
-        try {
-            Jwts.parserBuilder()
-                    .setSigningKey(key)
-                    .build()
-                    .parseClaimsJws(token);
-            return true;
-        } catch (SecurityException | MalformedJwtException | SignatureException e) {
-            log.error("Invalid JWT signature: {}", e.getMessage());
-            throw new BusinessException(ErrorCode.INVALID_ACCESS_TOKEN);
-        } catch (ExpiredJwtException e) {
-            log.error("JWT token is expired: {}", e.getMessage());
-            throw new BusinessException(ErrorCode.EXPIRED_TOKEN);
-        } catch (UnsupportedJwtException e) {
-            log.error("JWT token is unsupported: {}", e.getMessage());
-            throw new BusinessException(ErrorCode.INVALID_ACCESS_TOKEN);
-        } catch (IllegalArgumentException e) {
-            log.error("JWT claims string is empty: {}", e.getMessage());
-            throw new BusinessException(ErrorCode.INVALID_ACCESS_TOKEN);
-        }
-    }
-
-    // Access Token과 Refresh Token을 함께 재발급
-    public TokenPairResponse reissueTokens(String oldRefreshToken) {
-        // Refresh Token 검증
-        if (!validateToken(oldRefreshToken)) {
-            throw new JwtException("Invalid refresh token");
-        }
-
-        // Refresh Token에서 사용자 정보 추출
-        Claims claims = parseClaims(oldRefreshToken);
-        String email = claims.getSubject();
-
-        // CustomOAuth2UserService를 통해 사용자 정보를 다시 조회
-        CustomOAuth2User oAuth2User = customOAuth2UserService.loadUserByEmail(email);
-
-        Date now = new Date();
-        Date validity = new Date(now.getTime() + refreshTokenValidityInMilliseconds);
-
-        // attributes 확인을 위한 로그
-        log.info("OAuth2User attributes: {}", oAuth2User.getAttributes());
-        Long id;
-
-        Map<String, Object> attributes = oAuth2User.getAttributes();
-        id = Long.valueOf(attributes.get("id").toString());
-        String newRefreshToken = Jwts.builder()
-                .setSubject(email)
-                .setIssuedAt(now)
-                .setExpiration(validity)
-                .signWith(key, SignatureAlgorithm.HS256)
-                .compact();
-
-        String newAccessToken = Jwts.builder()
-                .setSubject(email)
-                .claim("id", id)
-                .claim("auth", oAuth2User.getAuthorities().stream()
-                        .map(GrantedAuthority::getAuthority)
-                        .collect(Collectors.joining(",")))
-                .setIssuedAt(now)
-                .setExpiration(validity)
-                .signWith(key, SignatureAlgorithm.HS256)
-                .compact();
-
-        return new TokenPairResponse(newAccessToken, newRefreshToken);
-    }
-
-    // Claims 파싱
-    public Claims parseClaims(String token) {
-        return Jwts.parserBuilder()
-                .setSigningKey(key)
-                .build()
-                .parseClaimsJws(token)
-                .getBody();
-    }
-}
+//
+//        return Jwts.builder()
+//                .setSubject(email)
+//                .setIssuedAt(now)
+//                .setExpiration(validity)
+//                .signWith(key, SignatureAlgorithm.HS256)
+//                .compact();
+//    }
+//
+//    // 토큰에서 Authentication 객체 추출
+//    public Authentication getAuthentication(String token) {
+//        Claims claims = parseClaims(token);
+//
+//        log.info("Token claims: {}", claims);
+//        log.info("id claim value: {}", claims.get("id"));
+//        log.info("subject value: {}", claims.getSubject());
+//        log.info("auth claim value: {}", claims.get("auth"));
+//
+//        Collection<? extends GrantedAuthority> authorities =
+//                Arrays.stream(claims.get("auth").toString().split(","))
+//                        .map(SimpleGrantedAuthority::new)
+//                        .collect(Collectors.toList());
+//
+//        CustomUserDetails principal = CustomUserDetails.builder()
+//                .id(Long.parseLong(claims.get("id").toString()))
+//                .email(claims.getSubject())
+//                .authorities(authorities)
+//                .build();
+//
+//        return new UsernamePasswordAuthenticationToken(principal, token, authorities);
+//    }
+//
+//    // 토큰 유효성 검증
+//    public boolean validateToken(String token) {
+//        try {
+//            Jwts.parserBuilder()
+//                    .setSigningKey(key)
+//                    .build()
+//                    .parseClaimsJws(token);
+//            return true;
+//        } catch (SecurityException | MalformedJwtException | SignatureException e) {
+//            log.error("Invalid JWT signature: {}", e.getMessage());
+//            throw new BusinessException(ErrorCode.INVALID_ACCESS_TOKEN);
+//        } catch (ExpiredJwtException e) {
+//            log.error("JWT token is expired: {}", e.getMessage());
+//            throw new BusinessException(ErrorCode.EXPIRED_TOKEN);
+//        } catch (UnsupportedJwtException e) {
+//            log.error("JWT token is unsupported: {}", e.getMessage());
+//            throw new BusinessException(ErrorCode.INVALID_ACCESS_TOKEN);
+//        } catch (IllegalArgumentException e) {
+//            log.error("JWT claims string is empty: {}", e.getMessage());
+//            throw new BusinessException(ErrorCode.INVALID_ACCESS_TOKEN);
+//        }
+//    }
+//
+//    // Access Token과 Refresh Token을 함께 재발급
+//    public TokenPairResponse reissueTokens(String oldRefreshToken) {
+//        // Refresh Token 검증
+//        if (!validateToken(oldRefreshToken)) {
+//            throw new JwtException("Invalid refresh token");
+//        }
+//
+//        // Refresh Token에서 사용자 정보 추출
+//        Claims claims = parseClaims(oldRefreshToken);
+//        String email = claims.getSubject();
+//
+//        // CustomOAuth2UserService를 통해 사용자 정보를 다시 조회
+//        CustomOAuth2User oAuth2User = customOAuth2UserService.loadUserByEmail(email);
+//
+//        Date now = new Date();
+//        Date validity = new Date(now.getTime() + refreshTokenValidityInMilliseconds);
+//
+//        // attributes 확인을 위한 로그
+//        log.info("OAuth2User attributes: {}", oAuth2User.getAttributes());
+//        Long id;
+//
+//        Map<String, Object> attributes = oAuth2User.getAttributes();
+//        id = Long.valueOf(attributes.get("id").toString());
+//        String newRefreshToken = Jwts.builder()
+//                .setSubject(email)
+//                .setIssuedAt(now)
+//                .setExpiration(validity)
+//                .signWith(key, SignatureAlgorithm.HS256)
+//                .compact();
+//
+//        String newAccessToken = Jwts.builder()
+//                .setSubject(email)
+//                .claim("id", id)
+//                .claim("auth", oAuth2User.getAuthorities().stream()
+//                        .map(GrantedAuthority::getAuthority)
+//                        .collect(Collectors.joining(",")))
+//                .setIssuedAt(now)
+//                .setExpiration(validity)
+//                .signWith(key, SignatureAlgorithm.HS256)
+//                .compact();
+//
+//        return new TokenPairResponse(newAccessToken, newRefreshToken);
+//    }
+//
+//    // Claims 파싱
+//    public Claims parseClaims(String token) {
+//        return Jwts.parserBuilder()
+//                .setSigningKey(key)
+//                .build()
+//                .parseClaimsJws(token)
+//                .getBody();
+//    }
+//}
