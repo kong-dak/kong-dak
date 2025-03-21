fc1fa6f43f6e400c17eda778e30896b7e620ff90
Changho0514 <chh9450@naver.com>
2025-03-21 16:38:38 +0900

    feat: NotificationService에 알림 전송 로직 추가 - 커플 매칭부분만 존재 했으나 아래 사항들이 추가됨   - Diary 작성, 일정 생성, 수정   - 데일리 질문 알림, 이모지 반응, 질문 답변, 답변 댓글   - 버킷리스트 생성, 완료

--- backend/src/main/java/com/kongdak/domain/notification/service/NotificationService.java
+++ backend/src/main/java/com/kongdak/domain/notification/service/NotificationService.java
@@ -51,10 +51,7 @@ public class NotificationService {
      * 알림 읽음 처리
      */
     @Transactional
-    public void markAsRead(
-            Long notificationId,
-            Long memberId
-    ) {
+    public void markAsRead(Long notificationId, Long memberId) {
         notificationRepository.findByIdAndReceiverId(notificationId, memberId)
                               .ifPresent(notification -> {
                                   notification.markAsRead();
@@ -66,12 +63,12 @@ public class NotificationService {
      * 커플 연결 요청 알림 전송
      */
     @Transactional
-    public void sendCoupleRequestNotification(
-            String requestId,
-            Long requesterId,
-            Long receiverId
-    ) {
-        NotificationEvent notification = NotificationEvent.ofRequest(NotificationType.COUPLE_MATCH_REQUEST, requestId, requesterId, receiverId);
+    public void sendCoupleRequestNotification(String requestId, Long requesterId, Long receiverId) {
+        NotificationEvent notification = NotificationEvent.createWithRequest(NotificationType.COUPLE_MATCH_REQUEST,
+                                                                             requestId,
+                                                                             requesterId,
+                                                                             receiverId)
+                                                          .build();
         sendNotification(notification);
     }
 
@@ -79,11 +76,13 @@ public class NotificationService {
      * 커플 연결 수락 알림 전송
      */
     @Transactional
-    public void sendCoupleAcceptedNotification(
-            Long requesterId,
-            Long receiverId
-    ) {
-        NotificationEvent notification = NotificationEvent.of(NotificationType.COUPLE_MATCH_ACCEPTED, receiverId, requesterId);
+    public void sendCoupleAcceptedNotification(Long requesterId, Long receiverId) {
+        NotificationEvent notification = NotificationEvent.create(NotificationType.COUPLE_MATCH_ACCEPTED,
+                                                                  requesterId,
+                                                                  receiverId
+                                                          )
+                                                          .build();
+
         sendNotification(notification);
     }
 
@@ -91,11 +90,12 @@ public class NotificationService {
      * 커플 연결 거절 알림 전송
      */
     @Transactional
-    public void sendCoupleRejectedNotification(
-            Long requesterId,
-            Long receiverId
-    ) {
-        NotificationEvent notification = NotificationEvent.of(NotificationType.COUPLE_MATCH_REJECTED, receiverId, requesterId);
+    public void sendCoupleRejectedNotification(Long requesterId, Long receiverId) {
+        NotificationEvent notification = NotificationEvent.create(NotificationType.COUPLE_MATCH_REJECTED,
+                                                                  requesterId,
+                                                                  receiverId
+                                                          )
+                                                          .build();
         sendNotification(notification);
     }
 
@@ -103,11 +103,161 @@ public class NotificationService {
      * 콕 찌르기 알림 전송
      */
     @Transactional
-    public void sendPokeNotification(
-            Long senderId,
-            Long receiverId
-    ) {
-        NotificationEvent notification = NotificationEvent.of(NotificationType.POKE, senderId, receiverId);
+    public void sendPokeNotification(Long senderId, Long receiverId) {
+        NotificationEvent notification = NotificationEvent.create(NotificationType.POKE, senderId, receiverId)
+                                                          .build();
+        sendNotification(notification);
+    }
+
+    /**
+     * 기념일 알림 전송
+     */
+    @Transactional
+    public void sendAnniversaryNotification(Long memberId1, Long memberId2) {
+
+
+        NotificationEvent notification1 = NotificationEvent.create(NotificationType.COUPLE_ANNIVERSARY, memberId1)
+                                                           .build();
+        NotificationEvent notification2 = NotificationEvent.create(NotificationType.COUPLE_ANNIVERSARY, memberId2)
+                                                           .build();
+
+        sendNotification(notification1);
+        sendNotification(notification2);
+    }
+
+
+    /**
+     * 일정 관련 알림 전송 (생성, 수정)
+     */
+    private NotificationEvent createScheduleNotification(NotificationType type,
+                                                         Long scheduleId,
+                                                         Long actorId,
+                                                         Long partnerId) {
+        return NotificationEvent.create(type, actorId, partnerId)
+                                .addData("scheduleId", scheduleId.toString())
+                                .build();
+    }
+
+    /**
+     * 일정 생성 알림 전송
+     */
+    @Transactional
+    public void sendScheduleCreatedNotification(Long scheduleId, Long creatorId, Long partnerId) {
+        NotificationEvent notification = createScheduleNotification(NotificationType.SCHEDULE_CREATED,
+                                                                    scheduleId,
+                                                                    creatorId,
+                                                                    partnerId);
+        sendNotification(notification);
+    }
+
+    /**
+     * 일정 수정 알림 전송
+     */
+    @Transactional
+    public void sendScheduleUpdatedNotification(Long scheduleId, Long updaterId, Long partnerId) {
+        NotificationEvent notification = createScheduleNotification(NotificationType.SCHEDULE_UPDATED,
+                                                                    scheduleId,
+                                                                    updaterId,
+                                                                    partnerId);
+        sendNotification(notification);
+    }
+
+    /**
+     * 일정 리마인더 알림 전송 (스케줄러에서 호출)
+     */
+    @Transactional
+    public void sendScheduleReminderNotification(Long scheduleId, List<Long> memberIds) {
+        for (Long memberId : memberIds) {
+            NotificationEvent notification = NotificationEvent.create(NotificationType.SCHEDULE_REMINDER, memberId)
+                                                              .addData("scheduleId", scheduleId.toString())
+                                                              .build();
+            sendNotification(notification);
+        }
+    }
+
+
+    /**
+     * 일기 작성 알림 전송
+     */
+    @Transactional
+    public void sendDiaryCreatedNotification(Long diaryId, Long authorId, Long partnerId) {
+        NotificationEvent notification = NotificationEvent.create(NotificationType.DIARY_CREATED, authorId, partnerId)
+                                                          .addData("diaryId", diaryId.toString())
+                                                          .build();
+        sendNotification(notification);
+    }
+
+    /**
+     * 오늘의 질문 이모지 반응 알림 전송
+     */
+    @Transactional
+    public void sendDailyQuestionEmojiNotification(Long answerId, Long reactorId, Long authorId) {
+        NotificationEvent notification = NotificationEvent.create(NotificationType.DAILY_QUESTION_EMOJI_CREATED, reactorId, authorId)
+                                                          .addData("answerId", answerId.toString())
+                                                          .build();
+        sendNotification(notification);
+    }
+
+    /**
+     * 오늘의 질문 알림 전송
+     */
+    @Transactional
+    public void sendDailyQuestionNotification(Long questionId, List<Long> memberIds) {
+        for (Long memberId : memberIds) {
+            NotificationEvent notification = NotificationEvent.create(NotificationType.DAILY_QUESTION_NEW, memberId)
+                                                              .addData("questionId", questionId.toString())
+                                                              .build();
+            sendNotification(notification);
+        }
+    }
+
+    /**
+     * 질문 답변 알림 전송
+     */
+    @Transactional
+    public void sendQuestionAnsweredNotification(Long questionId, Long answererId, Long partnerId) {
+        NotificationEvent notification = NotificationEvent.create(NotificationType.DAILY_QUESTION_ANSWERED,
+                                                                  answererId,
+                                                                  partnerId)
+                                                          .addData("questionId", questionId.toString())
+                                                          .build();
+        sendNotification(notification);
+    }
+
+    /**
+     * 답변 댓글 알림 전송
+     */
+    @Transactional
+    public void sendAnswerRepliedNotification(Long questionId, Long replierId, Long answererId) {
+        NotificationEvent notification = NotificationEvent.create(NotificationType.DAILY_QUESTION_REPLIED,
+                                                                  replierId,
+                                                                  answererId)
+                                                          .addData("questionId", questionId.toString())
+                                                          .build();
+        sendNotification(notification);
+    }
+
+    /**
+     * 버킷리스트 생성 알림 전송
+     */
+    @Transactional
+    public void sendBucketCreatedNotification(Long bucketId, Long creatorId, Long partnerId) {
+        NotificationEvent notification = NotificationEvent.create(NotificationType.BUCKET_CREATED, creatorId, partnerId)
+                                                          .addData("bucketId", bucketId.toString())
+                                                          .build();
+        sendNotification(notification);
+    }
+
+    /**
+     * 버킷리스트 완료 알림 전송
+     */
+    @Transactional
+    public void sendBucketCompletedNotification(Long bucketId, Long completerId, Long partnerId) {
+        NotificationEvent notification = NotificationEvent.create(NotificationType.BUCKET_COMPLETED,
+                                                                  completerId,
+                                                                  partnerId)
+                                                          .addData("bucketId", bucketId.toString())
+                                                          .build();
         sendNotification(notification);
     }
 }
